47fbe6c190c818c44f9dd19d8bcc067960a1d210
manudevops726 <amanasa726@gmail.com>
2025-05-01 16:00:03 +0530

    Update variable.tf

--- PRIMARY-US-east-1/variable.tf
+++ PRIMARY-US-east-1/variable.tf
@@ -1,7 +1,7 @@
 variable "rds-password" {
     description = "rds password"
     type = string
-    default = "srivardhan"
+    default = "manasa123"
   
 }
 variable "rds-username" {
@@ -13,7 +13,7 @@ variable "rds-username" {
 variable "ami" {
     description = "ami"
     type = string
-    default = "ami-04b70fa74e45c3917"
+    default = "ami-0e35ddab05955cf57"
   
 }
 variable "instance-type" {
@@ -25,7 +25,7 @@ variable "instance-type" {
 variable "key-name" {
     description = "keyname"
     type = string
-    default = "us-east-1"
+    default = "ap-south-1"
   
 }
 variable "backupr-retention" {
